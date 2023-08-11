feb81a06b3901c039e016ccce4fcc4aec40e7a55
yansaev1 <126662576+yansaev1@users.noreply.github.com>
2023-08-11 11:46:24 +0300

    Add files via upload

--- NewsApp/news/models.py
+++ NewsApp/news/models.py
@@ -1,43 +1,78 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models import Sum
 # Create your models here.
 
-class User(models.Model):
-    name = models.CharField(max_length=20)
-    password = models.CharField(max_length=100)
+
 
 
 class Author(models.Model):
-    rating = models.IntegerField()
-    user = models.OneToOneField(User, on_delete= models.CASCADE, primary_key= True)
+    rating = models.SmallIntegerField(default=0)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+
+    def update_rating(self):
+        posts_rating = Post.objects.filter(author=self).aggregate(result=Sum('rating')).get('result')
+        comments_rating = Comment.objects.filter(user=self.user).aggregate(result=Sum('rating')).get('result')
+        comment_post = Comment.objects.filter(post__author__user=self.user).aggregate(result=Sum('rating')).get('result')
+
+        self.rating = 3 * posts_rating + comments_rating + comment_post
+        self.save()
+
+
 
 
 class Category(models.Model):
-    category = models.CharField(max_length=40, unique= True)
+    category = models.CharField(max_length=40, unique=True)
 
-news = 'Новость'
-article = 'Статья'
+news = 'NE'
+article = 'AR'
+
+POST_TYPES = [
+    (news, 'новость'),
+    (article, 'статья')
+]
 
 
 class Post(models.Model):
-    author = models.OneToOneField(Author, on_delete = models.CASCADE, primary_key = True)
-    choice = models.CharField(max_length= 5, choices=[(news, "новость"),(article, "статья")])
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,primary_key=False)
+    choice = models.CharField(max_length=10, choices=[(news, "новость"),(article, "статья")])
     article_date = models.DateTimeField(auto_now_add=True)
-    category = models.ManyToManyField(Category)
-    head = models.CharField(max_length= 40)
-    text = models.TextField(max_length= 2000)
-    rating_news = models.IntegerField()
+    category = models.ManyToManyField(Category, through="PostCategory")
+    head = models.CharField(max_length=70)
+    text = models.TextField(max_length=2000)
+    rating_news = models.SmallIntegerField(default=0)
+
+    def like_post(self):
+        self.rating_news += 1
+        return self.rating_news
+
+    def dislike_post(self):
+        self.rating_news -= 1
+        return self.rating_news
+
+    def preview(self):
+       return self.text[0:124]
 
 
 
 class PostCategory(models.Model):
-    post = models.ManyToManyField(Post, Category)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
-    post_user_comm = models.OneToOneField(Post, User, on_delete= models.CASCADE)
-    text_comm = models.CharField(max_length= 255)
+    post_comm = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_comm = models.ForeignKey(User, on_delete=models.CASCADE)
+    text_comm = models.CharField(max_length=255)
     date_comm = models.DateTimeField(auto_now_add=True)
-    rating_comm = models.IntegerField()
+    rating_comm = models.SmallIntegerField(default=0)
+
+    def like_comm(self):
+        self.rating_comm += 1
+        return self.rating_comm
+
+    def dislike_comm(self):
+        self.rating_comm -= 1
+        return self.rating_comm
 
 
